1268409a4936898cce7efb967ee9943c6224daf3
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-05-15 11:26:32 +0200

    Add a real testcase that tests the new constructor

---
+++ Tests/Raven/RavenTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class RavenTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRavenClient()
+    {
+        $kernel = new AppKernel('prod', false);
+        $environment = $kernel->getEnvironment();
+        $raven = new \Kunstmaan\SentryBundle\Raven\Raven('http://public:secret@example.com/1', $environment);
+
+        $this->assertEquals($raven->project, 1);
+        $this->assertEquals($raven->servers, array('https://example.com/api/store/'));
+        $this->assertEquals($raven->public_key, 'public');
+        $this->assertEquals($raven->secret_key, 'secret');
+        $this->assertEquals($raven->getEnvironment(), $environment);
+
+    }
+}
